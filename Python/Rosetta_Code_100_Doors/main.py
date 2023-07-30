33f380730c852db4d064e786f5bac257b42c2eea
Code Crafters Corner <SharCoding@gmail.com>
2023-07-30 11:26:17 +0400

    Update 100 doors with optimized version
    - Loop through doors once.
    - Remove unnecessary code section and comments.
    - Add comment to annotate optimization.

--- Python/Rosetta_Code_100_Doors/main.py
+++ Python/Rosetta_Code_100_Doors/main.py
@@ -1,18 +1,10 @@
-""" 100 Doors
-
-- 100 doors in row
-- initially closed
-- make a 100 passes
-- 1st time - visit every door - toggle state
-- 2nd time - visit every 2nd door - toggle state
-- 3rd time - visit every 3rd door - toggle state
-- name the final state of each door
-"""
-
-door_state = [False] * 100
-
 for n in range(1, 101):
-    for j in range(n, 101, n):
-        door_state[j-1] = not door_state[j-1]
 
-    print("Door number {0} is {1}".format(n, "opened" if door_state[n-1] else "closed"))
+    # Check if the square root of nth number 
+    # has remainder when divided by 1.
+    # If yes, the nth door will be closed.
+    # If no, the nth door will be opened.
+    if n**0.5 % 1 == 0:
+        print(f"Door {n} is opened.")
+    else:
+        print(f"Door {n} is closed.")
