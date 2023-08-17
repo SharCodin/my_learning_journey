24e009bf6f473ba2701f75ce1c80613b45ba6591
Code Crafters Corner <SharCoding@gmail.com>
2023-08-17 12:55:00 +0400

    Update CSharp Hello World Program
    Add CSharp Variables Project

--- CSharp/CSharp Programming All-in-One Tutorial Series (6 HOURS!)/HelloWorld/Program.cs
+++ CSharp/CSharp Programming All-in-One Tutorial Series (6 HOURS!)/HelloWorld/Program.cs
@@ -5,6 +5,20 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // To use command arguments
+            // Console.WriteLine("Command: " + args[0])
+
+            // To allow for user input.
+            // Can also be used to prevent the console
+            // from exiting.
+            // Console.ReadLine()
+
+            // String interpolation
+            // string name = "James"
+            // Console.WriteLine($"Hello {name}!")
         }
     }
 }
+
+// Namespace -> Class -> Member -> Method -> Statements
